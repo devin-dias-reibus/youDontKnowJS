0d54a8cb86ada2e461c9432d154be7bfa69528e7
Anthony Giamboy <agiamboy@reibus.com>
2022-12-08 21:47:55 -0500

    created a forEach fn

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,14 +2,26 @@ console.log();
 
 // Firstly, let’s get this out of the way, FizzBuzz is a task where the programmer is asked to print numbers from 1 to 100, but here’s the catch, multiple of three should print “Fizz” and similarly print “Buzz” for multiples of 5 and lastly print “FizzBuzz” for multiples of three and five.
 
-for (let i = 1; i < 101; i++) {
-  if (i % 3 === 0 && i % 5 === 0) {
-    console.log("fizzbuzz");
-  } else if (i % 3 === 0) {
-    console.log("fizz");
-  } else if (i % 5 === 0) {
-    console.log("buzz");
-  } else {
-    console.log(i);
-  }
+// for (let i = 1; i < 101; i++) {
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     console.log("fizzbuzz");
+//   } else if (i % 3 === 0) {
+//     console.log("fizz");
+//   } else if (i % 5 === 0) {
+//     console.log("buzz");
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+function fizzbuzz(arr) {
+  arr.forEach((Number) => {
+    if (Number % 3 === 0 && Number % 5 === 0) {
+      console.log("fizzbuzz");
+    } else if (Number % 3 === 0) {
+      console.log("fizz");
+    } else if (Number % 5 === 0) {
+      console.log("buzz");
+    }
+  });
 }
