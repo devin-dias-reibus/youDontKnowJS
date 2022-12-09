e49c3f8e8f83c84b1ea66ea5b9e23de87888ffa4
Anthony Giamboy <agiamboy@reibus.com>
2022-12-08 21:40:57 -0500

    fizzbuzz working

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,3 +1,15 @@
 console.log();
 
 // Firstly, let’s get this out of the way, FizzBuzz is a task where the programmer is asked to print numbers from 1 to 100, but here’s the catch, multiple of three should print “Fizz” and similarly print “Buzz” for multiples of 5 and lastly print “FizzBuzz” for multiples of three and five.
+
+for (let i = 1; i < 101; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("fizzbuzz");
+  } else if (i % 3 === 0) {
+    console.log("fizz");
+  } else if (i % 5 === 0) {
+    console.log("buzz");
+  } else {
+    console.log(i);
+  }
+}
